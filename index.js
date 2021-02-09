926020f9bd032a64ce5918f2120101eaba0ed64b
Julian <julianrosenthal@yahoo.com>
2021-02-09 14:47:51 -0700

    commit tree 4e8558199607dce7bb7cb68486a20fe68cac2627

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names) {
+  let messages = []
+  for (let i = 0; i < names.length; i++) {
+    let nme = names[i];
+    let message = `Thank you, ${nme}, for the wonderful surprise gift!`;
+    messages.push(message);
+  }
+  return messages;
+}
+
+function countDown(number) {
+  for (let i = number; i >= 0; i--) {
+    console.log(i);
+  }
+}
